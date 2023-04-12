2812c856988bfcd35dce983fe2d9913a77762203
PedroCruz <contatocreed67@gmail.com>
2023-04-11 20:23:53 -0400

    CRUD created and tested

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
+import { carRoutes } from "./routes/cars.routes";
 
 const app = express();
 app.use(express.json());
+
+app.use("/cars", carRoutes);
 
 export default app;
