9324cdad04a9996e82917ff8693ad837fb5151bf
Rafael Quadros <contatorafaelquadros@gmail.com>
2023-04-19 13:45:07 -0300

    feat: create all users routes and protect routes for all

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import UsersController from "../controllers/users.controller";
 import ensureDataIsValidMiddleware from "../middlewares/ensureDataValid.middleware";
-import { CreateUserSerializer } from "../serializers/users.serializers";
+import {
+  CreateUserSerializer,
+  UpdateUserSerializer,
+} from "../serializers/users.serializers";
+import validQueryPaginationMiddleware from "../middlewares/pagination.middleware";
+import Middlewares from "../middlewares/auth.middlewares";
 
 export const usersRouter = Router();
 
@@ -10,4 +15,16 @@ usersRouter.post(
   ensureDataIsValidMiddleware(CreateUserSerializer),
   UsersController.create
 );
+usersRouter.get(
+  "/:id/cars",
+  validQueryPaginationMiddleware,
+  UsersController.listAllCars
+);
 usersRouter.get("/:id", UsersController.listOne);
+usersRouter.patch(
+  "",
+  Middlewares.Auth,
+  ensureDataIsValidMiddleware(UpdateUserSerializer),
+  UsersController.update
+);
+usersRouter.delete("", Middlewares.Auth, UsersController.delete);
--- src/serializers/users.serializers.ts
+++ src/serializers/users.serializers.ts
@@ -41,3 +41,25 @@ export const LoginUserSerializer: any = yup.object().shape({
   email: yup.string().required(),
   password: yup.string().required(),
 });
+
+export const UpdateAddressSerializers: any = yup.object().shape({
+  street: yup.string().notRequired(),
+  number: yup.string().notRequired(),
+  cep: yup.string().notRequired(),
+  city: yup.string().notRequired(),
+  state: yup.string().notRequired(),
+  complement: yup.string().notRequired(),
+});
+
+export const UpdateUserSerializer: any = yup.object().shape({
+  firstname: yup.string().notRequired(),
+  lastname: yup.string().notRequired(),
+  email: yup.string().email().notRequired(),
+  password: yup.string().notRequired(),
+  cpf: yup.string().notRequired(),
+  contact: yup.string().notRequired(),
+  isWhatsapp: yup.boolean().notRequired(),
+  birthdate: yup.string().notRequired(),
+  description: yup.string().notRequired(),
+  isSalesman: yup.boolean().notRequired(),
+});
