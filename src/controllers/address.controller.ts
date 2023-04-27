8806526e73ff4d80faba54ca257a1a410a573e8b
naoeoneto <abdpneto@gmail.com>
2023-04-27 12:04:31 -0300

    fix: update address logic

--- src/controllers/address.controller.ts
+++ src/controllers/address.controller.ts
@@ -4,9 +4,10 @@ import AddressService from "../services/address.service";
 class AddressController {
   static async update(req: Request, res: Response) {
     const userId: string = req.user.id;
+    const addressId: string = req.params.id
     const addressData = req.body;
 
-    const response: any = await AddressService.update(addressData, userId);
+    const response: any = await AddressService.update(addressData, userId, addressId);
 
     return res.status(200).json(response);
   }
--- src/services/address.service.ts
+++ src/services/address.service.ts
@@ -1,16 +1,25 @@
 import { Request, Response } from "express";
 import Address from "../entities/address.entity";
 import AppDataSource from "../data-source";
+import { Users } from "../entities/user.entity";
 
 class AddressService {
-  static async update(addressData: any, userId: string) {
+  static async update(addressData: any, userId: string, addressId: string) {
     const addressRepository = AppDataSource.getRepository(Address);
+    const userRepository = AppDataSource.getRepository(Users)
 
-    const address = await addressRepository.findOneBy({ id: userId });
+    const user = await userRepository.findOneBy({ id: userId })
+
+    // const address = await addressRepository.findOneBy({ id: userId });
+    const address = await addressRepository.findOne({
+      where: { id: addressId },
+      relations: { user: true },
+    });
 
     const addressUpdate: any = addressRepository.create({
       ...address,
       ...addressData,
+      user: user
     });
 
     await addressRepository.save(addressUpdate);
