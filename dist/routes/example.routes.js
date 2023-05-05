a74bd54e8562a50e66b14f171b1416faeac9c18a
KelvinSilva10 <kelvin.silva10@yahoo.com.br>
2023-05-05 14:44:12 -0300

    deleted dist

--- dist/routes/example.routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.exampleRouter = void 0;
-const express_1 = require("express");
-exports.exampleRouter = (0, express_1.Router)();
-exports.exampleRouter.post("");
